3bd069fb4c700721feddd72942c7603a69a265d2
Florian Ehmann <kontakt@florianehmann.de>
2024-08-26 22:07:56 +0200

    Display board as unicode

--- src/hexapawn/__init__.py
+++ src/hexapawn/__init__.py
@@ -1,6 +1,7 @@
 """Python Hexapawn library for playing and move generation"""
 
 from enum import Enum
+from itertools import product
 from typing import Optional, TypeAlias, Self
 
 Color: TypeAlias = bool
@@ -71,10 +72,47 @@ class Board:
         self.turn: Color = WHITE
         """Indicates which player does the next move"""
 
-    def reset(self) -> None:
+        self.reset()
+
+    def clear(self) -> Self:
+        """Removes all pieces from the board"""
+        self._board = [[None for _ in range(3)] for _ in range(3)]
+        for i, j in product(range(len(self._board)), range(len(self._board[0]))):
+            self._board[i][j] = None
+
+        return self
+
+    def reset(self) -> Self:
         """Restores the starting position"""
-        self._board = [[WHITE, None, BLACK] for _ in range(3)]
+        for square in [Squares.A1, Squares.B1, Squares.C1]:
+            file, rank = square.value
+            self._board[file][rank] = WHITE
+
+        for square in [Squares.A3, Squares.B3, Squares.C3]:
+            file, rank = square.value
+            self._board[file][rank] = BLACK
+
         self.turn = WHITE
+
+        return self
 
     def legal_moves(self):
         pass
+
+    def to_unicode(self) -> str:
+        """Returns a Unicode symbol representation of the state of the board"""
+        result = ""
+        for rank in range(len(self._board[0])):
+            for file in range(len(self._board)):
+                entry = self._board[file][rank]
+                if entry is not None:
+                    result += "♙ " if entry == WHITE else "♟ "
+                else:
+                    result += "□ " if (file + rank) % 2 != 0 else "■ "
+            result += "\n"
+        result = result[:-1]
+        return result
+
+    def print(self) -> None:
+        """Prints a Unicode representation if the state of the board"""
+        print(self.to_unicode())
